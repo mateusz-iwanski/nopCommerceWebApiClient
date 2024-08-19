a9e7cddd6c1712bc4bf7b59c5fad8d61e579e9b5
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-19 15:38:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace nopCommerceWebApiClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+           
+        }
+    }
+}
