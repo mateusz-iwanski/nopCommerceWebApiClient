c80767c699dfe8093fdafc427a07eb8f30666da2
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-19 22:17:46 +0200

    Create CreateAddressDto for creating address for individual person

---
+++ Objects/Address/AddressCreate.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+using System.Text.Json.Serialization;
+
+namespace nopCommerceWebApiClient.Objects.Address
+{
+    /// <summary>
+    /// CreateAddressDto uses for creating address for individual person
+    /// </summary>
+    public record AddressCreate : Address
+    {
+        private string _city;
+        private string _address1;
+        private int _countryId;
+
+        public override int Id { get; init; } = 0;
+        public override string City
+        {
+            get => _city;
+            init
+            {
+                _city = string.IsNullOrEmpty(value) ? throw new ArgumentNullException(nameof(City), "City cannot be null or empty") : value;
+            }
+        }
+        public override string Address1
+        {
+            get => _address1;
+            init
+            {
+                _address1 = string.IsNullOrEmpty(value) ? throw new ArgumentNullException(nameof(Address1), "Address1 cannot be null or empty") : value;
+            }
+        }
+        public new int CountryId
+        {
+            get => _countryId;
+            init
+            {
+                _countryId = value <= 0 ? throw new ArgumentOutOfRangeException(nameof(CountryId), "CountryId must be greater than 0") : value;
+            }
+        }
+    }
+}
