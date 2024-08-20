7a1f61c548a53d5899cdad83dff4e5403aad22cd
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-20 15:23:07 +0200

    Create AddressUpdatePolishEnterprise object

---
+++ Objects/Address/AddressUpdatePolishEnterprise.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+namespace nopCommerceWebApiClient.Objects.Address
+{
+    /// <summary>
+    /// Address update Data Transfer Object for Polish Enterprise updating
+    /// </summary>
+    public record AddressUpdatePolishEnterprise : AddressCreatePolishEnterprise
+    {
+        public required override int Id { get; init; }
+    }
+}
