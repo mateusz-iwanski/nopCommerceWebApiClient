b5a36592b42e57677c4dd4a610cc3780f58c0cf6
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-20 15:22:45 +0200

    Create AddressUpdate object

---
+++ Objects/Address/AddressUpdate.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+namespace nopCommerceWebApiClient.Objects.Address
+{
+    /// <summary>
+    /// Address Data Transfer Object for updating
+    /// </summary>
+    public record AddressUpdate : AddressCreate
+    {
+        public required override int Id { get; init; }
+    }
+}
