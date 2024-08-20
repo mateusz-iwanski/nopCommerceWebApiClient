343b493faeaf41169bef88e922d11263d122f1e8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-20 14:30:42 +0200

    Create AddressDetails object

---
+++ Objects/Address/AddressDetails.cs
@@ -0,0 +1,20 @@
+﻿using nopCommerceWebApiClient.Objects.Country;
+using nopCommerceWebApiClient.Objects.State;
+
+namespace nopCommerceWebApiClient.Objects.Address
+{
+    /// <summary>
+    /// Address details Data Transfer Object
+    /// </summary>
+    /// <remarks>
+    /// This object should be used only for the get method in controller
+    /// </remarks>
+    public record AddressDetails : Address
+    {
+        /// <summary>
+        /// ## NIP
+        /// ### Gets the Nip
+        /// </summary>
+        public string? NIP { get; init; }
+    }
+}
