25200f285e5b1dab337d706b16b9918531772cfe
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 08:46:11 +0200

    Create an object for the country service

---
+++ Objects/Country/Country.cs
@@ -0,0 +1,81 @@
+﻿
+
+
+namespace nopCommerceWebApiClient.Objects.Country
+{
+    /// <summary>
+    /// Country Data Transfer Object
+    /// </summary>
+    /// <remarks>
+    /// This object should be used only for the get method in controller
+    /// </remarks>
+    public record Country
+    {
+        public virtual int Id { get; init; }
+        /// <summary>
+        /// ## Name
+        /// ### Gets the name
+        /// </summary>
+        public required virtual string Name { get; init; }
+
+        /// <summary>
+        /// ## TwoLetterIsoCode
+        /// ### Gets a value indicating whether billing is allowed to this country
+        /// </summary>
+        //[Required]
+        public required virtual string TwoLetterIsoCode { get; init; }
+
+        /// <summary>
+        /// ## ThreeLetterIsoCode
+        /// ### Gets a value indicating whether shipping is allowed to this country
+        /// *Defaul = null*
+        /// </summary>
+        public virtual string? ThreeLetterIsoCode { get; init; }
+
+        /// <summary>
+        /// ## AllowsBilling
+        /// ### Gets the two letter ISO code
+        /// *Default = true*
+        /// </summary>
+        public virtual bool AllowsBilling { get; init; }
+
+        /// <summary>
+        /// ## AllowsShipping
+        /// ### Gets the three letter ISO code
+        /// *Default = false*
+        /// </summary>
+        public virtual bool AllowsShipping { get; init; }
+
+        /// <summary>
+        /// ## NumericIsoCode
+        /// ### Gets the numeric ISO code
+        /// </summary>
+        public virtual int NumericIsoCode { get; init; }
+
+        /// <summary>
+        /// ## SubjectToVat
+        /// ### Gets a value indicating whether customers in this country must be charged EU VAT
+        /// </summary>
+        public virtual bool SubjectToVat { get; init; }
+
+        /// <summary>
+        /// ## Published
+        /// ### Gets a value indicating whether the entity is published
+        /// </summary>
+        public virtual bool Published { get; init; }
+
+        /// <summary>
+        /// ## DisplayOrder
+        /// ### Gets the display order
+        /// *Default = 1*
+        /// </summary>
+        public virtual int DisplayOrder { get; init; }
+
+        /// <summary>
+        /// ## LimitedToStores
+        /// ### Gets a value indicating whether the entity is limited/restricted to certain stores
+        /// *Default = 0*
+        /// </summary>
+        public virtual bool LimitedToStores { get; init; }
+    }
+}
