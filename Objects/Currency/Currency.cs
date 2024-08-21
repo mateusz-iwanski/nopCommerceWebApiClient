20078e606d9fda2ea89513f10aaeb4080bcc2e70
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 08:48:58 +0200

    Create an object for the currency service

---
+++ Objects/Currency/Currency.cs
@@ -0,0 +1,29 @@
+﻿namespace nopCommerceWebApiClient.Objects.Currency
+{
+    public record Currency 
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public string CurrencyCode { get; init; }
+
+        public string? DisplayLocale { get; init; }
+
+        public string? CustomFormatting { get; init; }
+
+        public decimal Rate { get; init; }
+
+        public bool LimitedToStores { get; init; }
+
+        public bool Published { get; init; }
+
+        public int DisplayOrder { get; init; }
+
+        public DateTime CreatedOnUtc { get; init; }
+
+        public DateTime UpdatedOnUtc { get; init; }
+
+        public int RoundingTypeId { get; init; }        
+    }
+}
