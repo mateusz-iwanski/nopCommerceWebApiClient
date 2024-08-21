8dc903be98f42ce0d1f4fc47bf7103d1935f49ed
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 08:45:40 +0200

    Create an interface for the country service

---
+++ Interfaces/Country/ICountryService.cs
@@ -0,0 +1,13 @@
+﻿
+using nopCommerceWebApiClient.Objects.Country;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Country
+{
+    public interface ICountryService
+    {
+        [Get("/api/country")]
+        Task<IEnumerable<Objects.Country.Country>> GetAllAsync();
+    }
+
+}
