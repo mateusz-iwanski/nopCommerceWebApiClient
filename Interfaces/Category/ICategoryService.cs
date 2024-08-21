405116648590493cf1a93ea58665cc6e95b6f6b9
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 08:43:14 +0200

    Create an interface for the category service

---
+++ Interfaces/Category/ICategoryService.cs
@@ -0,0 +1,25 @@
+﻿using nopCommerceWebApiClient.Objects.Category;
+
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Category
+{
+    public interface ICategoryService
+    {
+        [Post("/api/category")]
+        Task<Objects.Category.Category> CreateAsync(CategoryCreate createCategoryDto);
+
+        [Delete("/api/category/{id}")]
+        Task DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/category")]
+        Task<IEnumerable<Objects.Category.Category>> GetAllAsync();
+
+        [Get("/api/category/{id}")]
+        Task<Objects.Category.Category> GetByIdAsync([AliasAs("id")] int id);
+
+        [Put("/api/category")]
+        Task<Objects.Category.Category> UpdateAsync(CategoryUpdate updateCategoryDto);
+    }
+    
+}
