8b25a7de420f0d9e5d105914ab7e2274cc1f29a3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 08:34:45 +0200

    Create ValidationErrorResponse for deserialize data from HTTP content (as a string) with validation errors

---
+++ Helpers/ValidationErrorResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace nopCommerceWebApiClient.Helpers
+{
+    /// <summary>
+    /// <c>ValidationErrorResponse</c> class.
+    /// </summary>
+    /// <remarks>
+    /// Used to deserialize data from HTTP content (as a string) with validation errors.
+    /// </remarks>
+    public class ValidationErrorResponse
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("errors")]
+        public Dictionary<string, string[]> Errors { get; set; }
+
+        [JsonPropertyName("traceId")]
+        public string TraceId { get; set; }
+    }
+}
