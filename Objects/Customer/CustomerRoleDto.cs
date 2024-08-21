9c8feaad0f27c7723b2bb9ae26979dde3c830bef
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 16:09:34 +0200

    Create CustomerRole DTO

---
+++ Objects/Customer/CustomerRoleDto.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace nopCommerceWebApiClient.Objects.Customer
+{
+    /// <summary>
+    /// The customer roles in nopCommerce enable you to form groups of your web store users. 
+    /// You can create various groups, such as store admins, shoppers, vendors, and others. 
+    /// 
+    /// If you don't need this option just leave this field empty. 
+    /// In order to use this functionality, you have to disable the following 
+    /// setting: Configuration > Settings > Catalog > Ignore ACL rules (sitewide).
+    /// </summary>
+
+    public record CustomerRoleDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? SystemName { get; set; }
+        public bool FreeShipping {  get; set; }
+        public bool TaxExempt { get; set; }
+        public bool Active { get; set; }
+        public bool IsSystemRole { get; set; }
+        public bool EnablePasswordLifetime { get; set; }
+        public bool OverrideTaxDisplayType { get; set; }
+        public int DefaultTaxDisplayTypeId { get; set; }
+        public int PurchasedWithProductId { get; set; }
+    }
+}
