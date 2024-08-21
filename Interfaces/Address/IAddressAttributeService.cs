65d49200dccc31a22514e0345497f01da6eaf794
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 08:41:15 +0200

    Create interface for address attribute service

---
+++ Interfaces/Address/IAddressAttributeService.cs
@@ -0,0 +1,13 @@
+﻿using nopCommerceWebApiClient.Objects.AddressAttribute;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Address
+{
+    public interface IAddressAttributeService
+    {
+        [Get("/api/addressattribute")]
+        Task<IEnumerable<AddressAttribute>> GetAllAsync();
+    }
+
+    
+}
