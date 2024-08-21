a6d221721b1af1e65c7c5df7d09d4ce058f1c63e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-21 16:09:06 +0200

    Create CustomerRoleService

---
+++ Interfaces/Customer/CustomerRoleService.cs
@@ -0,0 +1,11 @@
+﻿using nopCommerceWebApiClient.Objects.Customer;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Customer
+{
+    public interface ICustomerRoleService
+    {
+        [Get("/api/customer/role")]
+        Task<IEnumerable<CustomerRoleDto>> GetAllAsync();
+    }
+}
