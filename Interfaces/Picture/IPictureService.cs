b37ad3b6a2bc9dd756d04a749dde906d0c1e997c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 15:54:18 +0200

    Create a picture interface

---
+++ Interfaces/Picture/IPictureService.cs
@@ -0,0 +1,27 @@
+﻿using nopCommerceWebApiClient.Objects.Picture;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Picture
+{
+    public interface IPictureService
+    {
+        [Post("/api/picture")]
+        Task<PictureDto> CreateAsync(PictureCreateDto pictureCreateDto);
+
+        [Delete("/api/picture/{id}")]
+        Task<bool> DeleteAsync(int id);
+
+        [Get("/api/picture")]
+        Task<IEnumerable<PictureDto>> GetAllAsync();
+
+        [Get("/api/picture/{id}")]
+        Task<PictureDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Put("/api/picture")]
+        Task UpdateAsync(PictureUpdateDto pictureUpdateDto);
+
+        [Get("/api/picture/proper-name/{pictureId}")]
+        Task<string> ProperNameForPictureFileAsync(int pictureId);
+    }
+    
+}
