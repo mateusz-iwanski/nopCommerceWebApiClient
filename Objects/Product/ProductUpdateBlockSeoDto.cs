8451845c1848973d0a3a14b4566c7908f1234029
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 16:07:42 +0200

    Create a DTO update for a block SEO in the product object

---
+++ Objects/Product/ProductUpdateBlockSeoDto.cs
@@ -0,0 +1,23 @@
+﻿namespace nopCommerceWebApiClient.Objects.Product
+{
+    public record ProductUpdateBlockSeoDto
+    {
+        /// <summary>
+        /// ## MetaKeywords 
+        /// ### Set the meta keywords.
+        /// </summary>
+        public virtual string? MetaKeywords { get; init; }
+
+        /// <summary>
+        /// ## MetaTitle
+        /// ### Set the meta description.
+        /// </summary>
+        public virtual string? MetaTitle { get; init; }
+
+        /// <summary>
+        /// ## MetaDescription
+        /// ### Meta description to be added to product page header.
+        /// </summary>
+        public virtual string? MetaDescription { get; init; }
+    }
+}
