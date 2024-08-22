d562e9c35e0dd9d9a941b22cfaf00912f6bbd0ac
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 16:07:20 +0200

    Create a DTO update for a block rental in the product object

---
+++ Objects/Product/ProductUpdateBlockRentalPriceDto.cs
@@ -0,0 +1,33 @@
+﻿namespace nopCommerceWebApiClient.Objects.Product
+{
+    public record ProductUpdateBlockRentalPriceDto
+    {
+        /// <summary>
+        /// ## IsRental
+        /// ### Set a value indicating whether the product is rental
+        /// #### Check if this is a rental product (price is set for some period). Please note that inventory management is not fully 
+        /// #### supported for rental products yet. It's recommended to set 'Manage inventory method' to 'Don't track inventory' now.
+        /// *Default = false*
+        /// </summary>
+        public virtual bool IsRental { get; init; }
+
+        /// <summary>
+        /// ## RentalPriceLength
+        /// ### Set the rental length for some period (price for this period)
+        /// #### Price is specified for this period.
+        /// *Default = 1*
+        /// </summary>
+        public virtual int RentalPriceLength { get; init; }
+
+        /// <summary>
+        /// ## RentalPricePeriodId
+        /// ### Set the rental period (price for this period)
+        /// ** Defines the cycle period for a rental period **
+        /// * Days (0): The cycle is in days. (default setting)
+        /// * Weeks (10): The cycle is in weeks.
+        /// * Months (20): The cycle is in months.
+        /// * Years (30): The cycle is in years.
+        /// </summary>
+        public virtual int RentalPricePeriodId { get; init; }
+    }
+}
