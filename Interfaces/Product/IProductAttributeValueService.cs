d4940e8ba3a93979f37c5b784d4fd89ab3d3df6c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 20:05:28 +0200

    Create a product attribute value service

---
+++ Interfaces/Product/IProductAttributeValueService.cs
@@ -0,0 +1,24 @@
+﻿using nopCommerceWebApiClient.Objects.ProductAttributeValue;
+using Refit;
+using System;
+
+namespace nopCommerceWebApiClient.Interfaces.Product
+{
+    public interface IProductAttributeValueService
+    {
+        [Post("/api/product/attribute/value/add-to-attribute/{attributeId}")]
+        Task<HttpResponseMessage> CreateAsync([AliasAs("attributeId")] int attributeId, ProductAttributeValueCreateDto productAttributeValueCreateDto);
+
+        [Delete("/api/product/attribute/value/{id}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/product/attribute/value")]
+        Task<IEnumerable<ProductAttributeValueDto>> GetAllAsync();
+
+        [Get("/api/product/attribute/value/{id}")]
+        Task<ProductAttributeValueDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Put("/api/product/attribute/value")]
+        Task<HttpResponseMessage> UpdateAsync(ProductAttributeValueUpdateDto productAttributeValueUpdateDto);
+    }
+}
