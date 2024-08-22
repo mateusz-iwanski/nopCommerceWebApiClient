f099877441f8cafdfb0084b09d539e4022c1d259
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 15:59:43 +0200

    Create manufacturer DTO to update new one

---
+++ Objects/Manufacturer/ManufacturerUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace nopCommerceWebApiClient.Objects.Manufacturer
+{
+    public record ManufacturerUpdateDto : ManufacturerDto
+    {        
+        public required new int Id { get; init; }
+        public override DateTime CreatedOnUtc { get; init; }
+
+        public new DateTime UpdatedOnUtc { get; } = DateTime.Now;
+    }
+}
