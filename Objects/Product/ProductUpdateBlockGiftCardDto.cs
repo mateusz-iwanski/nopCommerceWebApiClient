15a0b41e061a4d9344536f21d49bbc78f8c2f598
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 16:05:55 +0200

    Create a DTO update for a block gift in the product object

---
+++ Objects/Product/ProductUpdateBlockGiftCardDto.cs
@@ -0,0 +1,30 @@
+﻿namespace nopCommerceWebApiClient.Objects.Product
+{
+    public record ProductUpdateBlockGiftCardDto
+    {
+        /// <summary>
+        /// ## IsGiftCard
+        /// ### Set a value indicating whether the product is gift card.
+        /// #### Check if it is a gift card. After adding gift card products to the shopping cart and completing the purchases, 
+        /// #### you can then search and view the list of all the purchased gift cards by selecting Gift Cards from the Sales menu.
+        /// *Default = false*
+        /// </summary>
+        public virtual bool IsGiftCard { get; init; }
+
+        /// <summary>
+        /// ## GiftCardTypeId
+        /// ### Set the gift card type identifier.
+        /// ** Note: Defines the type of gift card **
+        /// * virtual (0): A virtual gift card. (Default setting)
+        /// * Physical (1): A physical gift card.
+        /// </summary>
+        public virtual int GiftCardTypeId { get; init; }
+
+        /// <summary>
+        /// ## OverriddenGiftCardAmount
+        /// ### Set gift card amount that can be used after purchase. If not specified, then product price will be used.
+        /// *Default = null*
+        /// </summary>
+        public virtual decimal? OverriddenGiftCardAmount { get; init; }
+    }
+}
