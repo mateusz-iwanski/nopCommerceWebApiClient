496b03d7b21ec5d2f16f0e1b01c38310bc3f576d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 16:05:17 +0200

    Create a DTO update for a block attribute in the product object

---
+++ Objects/Product/ProductUpdateBlockAttributeDto.cs
@@ -0,0 +1,20 @@
+﻿namespace nopCommerceWebApiClient.Objects.Product
+{
+    public record ProductUpdateBlockAttributeDto
+    {
+        /// <summary>
+        /// ## AllowAddingOnlyExistingAttributeCombinations
+        /// ### Set a value indicating whether we allow adding to the cart/wishlist only attribute combinations that exist and have stock greater than zero.
+        /// #### This option is used only when we have "manage inventory" set to "track inventory by product attributes"
+        /// *Default = false*
+        /// </summary>
+        public virtual bool AllowAddingOnlyExistingAttributeCombinations { get; init; }
+
+        /// <summary>
+        /// ## DisplayAttributeCombinationImagesOnly
+        /// ### Set a value indicating whether to display attribute combination images only
+        /// *Default = false*
+        /// </summary>
+        public virtual bool DisplayAttributeCombinationImagesOnly { get; init; }
+    }
+}
