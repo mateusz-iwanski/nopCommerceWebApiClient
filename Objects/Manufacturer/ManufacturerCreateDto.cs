3a8c806047f9a520da5f3b56ba793ad3ef7c1c97
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 15:59:33 +0200

    Create manufacturer DTO to create  new one

---
+++ Objects/Manufacturer/ManufacturerCreateDto.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace nopCommerceWebApiClient.Objects.Manufacturer
+{
+    public record ManufacturerCreateDto : ManufacturerDto
+    {
+        public new int Id { get; } = 0;
+        
+        public new DateTime CreatedOnUtc { get; } = DateTime.Now;
+
+        public new DateTime UpdatedOnUtc { get; } = DateTime.Now;
+    }
+}
