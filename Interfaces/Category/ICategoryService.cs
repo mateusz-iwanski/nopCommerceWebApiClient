3cc719f2b4682aa3e7c8ec6fbf0f721df2a1b30c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 20:02:20 +0200

    Change the return object in the category interface

--- Interfaces/Category/ICategoryService.cs
+++ Interfaces/Category/ICategoryService.cs
@@ -7,10 +7,10 @@ namespace nopCommerceWebApiClient.Interfaces.Category
     public interface ICategoryService
     {
         [Post("/api/category")]
-        Task<Objects.Category.CategoryDto> CreateAsync(CategoryCreateDto createCategoryDto);
+        Task<HttpResponseMessage> CreateAsync(CategoryCreateDto createCategoryDto);
 
         [Delete("/api/category/{id}")]
-        Task DeleteAsync([AliasAs("id")] int id);
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
 
         [Get("/api/category")]
         Task<IEnumerable<Objects.Category.CategoryDto>> GetAllAsync();
