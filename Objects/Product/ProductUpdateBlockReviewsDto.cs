417cd2e29ffffc7d0b8560ea5ad6de019253c4f3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 16:07:31 +0200

    Create a DTO update for a block reviews in the product object

---
+++ Objects/Product/ProductUpdateBlockReviewsDto.cs
@@ -0,0 +1,32 @@
+﻿namespace nopCommerceWebApiClient.Objects.Product
+{
+    public record ProductUpdateBlockReviewsDto
+    {
+        /// <summary>
+        /// ## AllowCustomerReviews
+        /// ### Set a value indicating whether the product allows customer reviews.
+        /// #### By default, the reviews must be approved by the store owner before they appear in the public store. 
+        /// #### However, in case a store owner decides that reviews do not have to be approved, this default behavior can be changed. 
+        /// #### To opt out of the obligatory product reviews' approval, go to Configuration → Settings → Catalog settings and clear the Product reviews must be approved option.
+        /// *Default = false*
+        /// </summary>
+
+        public virtual bool AllowCustomerReviews { get; init; }
+
+        /// <summary>
+        /// ## ApprovedTotalReviews
+        /// ### Set the total rating votes (approved reviews).
+        /// #### Look on Configuration → Settings → Catalog settings for more details.
+        /// *Default = 0 (not approved)*
+        /// </summary>
+        public virtual int ApprovedTotalReviews { get; init; }
+
+        /// <summary>
+        /// ## NotApprovedTotalReviews
+        /// ### Set the total rating votes (not approved reviews).
+        /// #### Look on Configuration → Settings → Catalog settings for more details.
+        /// *Default = 0 (not approved)*
+        /// </summary>
+        public virtual int NotApprovedTotalReviews { get; init; }
+    }
+}
