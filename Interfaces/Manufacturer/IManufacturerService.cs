6f25825fb18b399b6db2ede32ed5dde3136e4457
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 15:54:05 +0200

    Create a manufacturer interface

---
+++ Interfaces/Manufacturer/IManufacturerService.cs
@@ -0,0 +1,24 @@
+﻿
+using nopCommerceWebApiClient.Objects.Manufacturer;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Manufacturer
+{
+    public interface IManufacturerService
+    {
+        [Post("/api/manufacturer")]
+        Task<ManufacturerDto> CreateAsync(ManufacturerCreateDto manufacturerCreateDto);
+
+        [Get("/api/manufacturer")]
+        Task<IEnumerable<ManufacturerDto>> GetAllAsync();
+
+        [Put("/api/manufacturer")]
+        Task<ManufacturerDto> UpdateAsync(ManufacturerUpdateDto manufacturerUpdateDto);
+
+        [Get("/api/manufacturer/{id}")]
+        Task<ManufacturerDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Delete("/api/manufacturer/{id}")]
+        Task<bool> DeleteAsync([AliasAs("id")] int id);
+    }
+}
