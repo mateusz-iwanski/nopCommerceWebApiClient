3750d0b70dd50c8fe833bc763088dc49b081d1c7
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 16:16:05 +0200

    Create a product attribute interface

---
+++ Interfaces/Product/IProductAttributeService.cs
@@ -0,0 +1,21 @@
+﻿using nopCommerceWebApiClient.Objects.ProductAttribute;
+using nopCommerceWebApiClient.Objects.ProductAttributeMapping;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Product
+{
+    public interface IProductAttributeService
+    {
+        [Post("/api/product-attributes")]
+        Task<(ProductAttributeDto, ProductProductAttributeMappingDto)> CreateAsync(ProductAttributeWithMappingCreateDto productAttributeWithMappingCreateDto);
+
+        [Get("/api/product-attributes")]
+        Task<IEnumerable<ProductAttributeDto>> GetAllAsync();
+
+        [Get("/api/product-attributes/{id}")]
+        Task<ProductAttributeDto> GetByIdAsync(int id);
+
+        [Put("/api/product-attributes")]
+        Task UpdateAsync(ProductAttributeUpdateDto productAttributeDtoUpdate);
+    }    
+}
