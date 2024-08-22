be5bf7b2da015f25f68194fa23792e865ebf5743
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 15:53:50 +0200

    Create a language interface

---
+++ Interfaces/Language/ILanguageService.cs
@@ -0,0 +1,11 @@
+﻿using nopCommerceWebApiClient.Objects.Language;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Language
+{
+    public interface ILanguageService
+    {
+        [Get("/api/language")]
+        Task<IEnumerable<LanguageDto>> GetAllAsync();
+    }
+}
