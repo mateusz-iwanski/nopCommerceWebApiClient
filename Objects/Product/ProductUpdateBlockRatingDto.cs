67fe069d6628ee2a4bd026e4642edbcfa4026592
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-22 16:06:55 +0200

    Create a DTO update for a block rating in the product object

---
+++ Objects/Product/ProductUpdateBlockRatingDto.cs
@@ -0,0 +1,22 @@
+﻿namespace nopCommerceWebApiClient.Objects.Product
+{
+    public record ProductUpdateBlockRatingDto
+    {
+        /// <summary>
+        /// ## ApprovedRatingSum
+        /// ### Set the rating sum (approved reviews).
+        /// #### Look on Configuration → Settings → Catalog settings for more details.
+        /// #### When required is set in new review type, customers have to choose an appropriate rating value before they can continue.
+        /// *Default = 0 (not approved)*
+        /// </summary>
+        public virtual int ApprovedRatingSum { get; init; }
+
+        /// <summary>
+        /// ## NotApprovedRatingSum
+        /// ### Set the rating sum (not approved reviews).
+        /// #### Look on Configuration → Settings → Catalog settings for more details.
+        /// *Default = 0 (not approved)*
+        /// </summary>
+        public virtual int NotApprovedRatingSum { get; init; }
+    }
+}
