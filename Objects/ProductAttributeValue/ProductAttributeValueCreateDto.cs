2359302d519454b7ccdaad0d5840538558e05cf3
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-23 11:26:52 +0200

    Create the ProductAttributeValue DTO for creating

---
+++ Objects/ProductAttributeValue/ProductAttributeValueCreateDto.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace nopCommerceWebApiClient.Objects.ProductAttributeValue
+{
+    public record ProductAttributeValueCreateDto : ProductAttributeValueDto
+    {
+        public new int Id { get; }
+
+        /// <summary>
+        /// ## ProductAttributeMappingId
+        /// ### Gets the product attribute mapping identifier
+        /// #### Gets product id which is associate with this attribute value
+        /// #### It's id of product
+        /// #### It's set by web api service
+        /// </summary>
+        public new int ProductAttributeMappingId { get; }
+    }
+}
