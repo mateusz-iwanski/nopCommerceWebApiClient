33af283f651002111afdce8a18406a5339d4912a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-23 11:25:11 +0200

    Create the ProductAttribute DTO for updating

---
+++ Objects/ProductAttribute/ProductAttributeUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace nopCommerceWebApiClient.Objects.ProductAttribute
+{
+    public record ProductAttributeUpdateDto : ProductAttributeDto
+    {
+        public required new int Id { get; init; }   
+    }
+}
