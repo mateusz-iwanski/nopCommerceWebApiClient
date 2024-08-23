e4f5c7929dee10c272e0c0113427665a70b3fdd6
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-23 11:21:58 +0200

    Create the ProductAvailabilityRangeService interface

---
+++ Interfaces/Product/IProductAvailabilityRangeService.cs
@@ -0,0 +1,20 @@
+﻿using nopCommerceWebApiClient.Objects.ProductAvailabilityRange;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Product
+{
+    public interface IProductAvailabilityRangeService
+    {
+        [Post("/api/product/availabilityrange")]
+        Task<HttpResponseMessage> CreateAsync(ProductAvailabilityRangeCreateDto productAvailabilityRangeDto);
+
+        [Get("/api/product/availabilityrange")]
+        Task<IEnumerable<ProductAvailabilityRangeDto>> GetAllAsync();
+
+        [Get("/api/product/availabilityrange/{id}")]
+        Task<ProductAvailabilityRangeDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Delete("/api/product/availabilityrange/{id}")]
+        Task<HttpResponseMessage> DeleteAsync(int id);
+    }
+}
