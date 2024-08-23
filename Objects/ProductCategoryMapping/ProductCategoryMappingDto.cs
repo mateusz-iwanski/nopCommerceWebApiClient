62fa580a6dd93d9d8956221bd051146ffac0609b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-23 11:28:11 +0200

    Create the ProductCategoryMapping DTO

---
+++ Objects/ProductCategoryMapping/ProductCategoryMappingDto.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace nopCommerceWebApiClient.Objects.ProductCategory
+{
+    /// <summary>
+    /// Product category mapping DTO
+    /// </summary>
+    /// <remarks>
+    /// Associate a product with a category
+    /// </remarks>
+    public record ProductCategoryMappingDto
+    {
+
+        public virtual int Id { get; init; }
+
+        /// <summary>
+        /// Gets or set the category identifier
+        /// </summary>
+        public required virtual int CategoryId { get; init; }
+
+        /// <summary>
+        /// Gets or set the product identifier
+        /// </summary>
+        public required virtual int ProductId { get; init; }
+
+        /// <summary>
+        /// Gets or set a value indicating whether the product is featured
+        /// *Default = false*
+        /// </summary>
+        public virtual bool IsFeaturedProduct { get; init; }
+
+        /// <summary>
+        /// Gets or set the display order        
+        /// </summary>
+        /// <remarks>
+        /// *Default = false*
+        /// </remarks>
+        public virtual int DisplayOrder { get; init; }
+
+    }
+}
