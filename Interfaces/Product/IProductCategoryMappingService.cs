062fe8c53d2e5b932ca05e044d5a2311f5ad8f91
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-23 11:22:09 +0200

    Create the ProductAvailabilityRangeService interface

---
+++ Interfaces/Product/IProductCategoryMappingService.cs
@@ -0,0 +1,23 @@
+﻿using nopCommerceWebApiClient.Objects.ProductCategory;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Product
+{
+    public interface IProductCategoryMappingService
+    {
+        [Post("/api/category/mapping")]
+        Task<ProductCategoryMappingDto> CreateAsync(ProductCategoryMappingCreateDto productCategoryMappingCreateDto);
+
+        [Delete("/api/category/mapping/product/{productId}/category/{categoryId}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("productId")] int productId,[AliasAs("categoryId")] int categoryId);
+
+        [Get("/api/category/mapping")]
+        Task<IEnumerable<ProductCategoryMappingDto>> GetAllAsync();
+
+        [Get("/api/category/mapping/product/{productId}")]
+        Task<IEnumerable<ProductCategoryMappingDto>> GetAllAssociateWithProductAsync([AliasAs("productId")] int productId);
+
+        [Get("/api/category/mapping/category/{categoryId}")]
+        Task<IEnumerable<ProductCategoryMappingDto>> GetAllAssociateWithCategoryAsync([AliasAs("categoryId")] int categoryId);
+    }
+}
