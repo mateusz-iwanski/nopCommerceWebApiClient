67b0f5f2c0ee2a97fe4b1d3e805e56a2fcb8ace8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:05:16 +0200

    Create the SpecificationAttributeGroupService interface

---
+++ Interfaces/SpecificationAttribute/ISpecificationAttributeGroupService.cs
@@ -0,0 +1,26 @@
+﻿using nopCommerceWebApiClient.Objects.SpecyficationAttributeGroup;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.SpecificationAttribute
+{
+    public interface ISpecificationAttributeGroupService
+    {
+        [Post("/api/specification-attribute/group")]
+        Task<SpecificationAttributeGroupDto> CreateAsync(SpecificationAttributeGroupCreateDto specificationAttributeGroupCreateDto);
+
+        [Delete("/api/specification-attribute/group/{id}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/specification-attribute/group")]
+        Task<IEnumerable<SpecificationAttributeGroupDto>> GetAllAsync();
+
+        [Get("/api/specification-attribute/group/{id}")]
+        Task<SpecificationAttributeGroupDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Get("/api/specification-attribute/group/name/{name}")]
+        Task<SpecificationAttributeGroupDto> GetByNameAsync(string name);
+
+        [Put("/api/specification-attribute/group")]
+        Task<HttpResponseMessage> UpdateAsync(SpecificationAttributeGroupUpdateDto specificationAttributeGroupUpdateDto);
+    }
+}
