d13bfbd8ee5b62ba8bf0becf08632978193dc81b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 11:38:55 +0200

    Create the ProductSpecificationAttributeMappingService interface

---
+++ Interfaces/Product/IProductSpecificationAttributeMappingService.cs
@@ -0,0 +1,26 @@
+﻿using nopCommerceWebApiClient.Objects.ProductSpecificationAttributeMapping;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Product
+{
+    public interface IProductSpecificationAttributeMappingService
+    {
+        [Post("/api/product/specification-attribute/mapping")]
+        Task<HttpResponseMessage> CreateAsync(ProductSpecificationAttributeMappingCreateDto productSpecificationAttributeMappingCreateDto);
+
+        [Delete("/api/product/specification-attribute/mapping/{id}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/product/specification-attribute/mapping")]
+        Task<IEnumerable<ProductSpecificationAttributeMappingDto>> GetAllAsync();
+
+        [Get("/api/product/specification-attribute/mapping/{id}")]
+        Task<ProductSpecificationAttributeMappingDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Get("/api/product/specification-attribute/mapping/product/{productId}")]
+        Task<IEnumerable<ProductSpecificationAttributeMappingDto>> GetByProductIdAsync([AliasAs("productId")] int productId);
+
+        [Put("/api/product/specification-attribute/mapping/")]
+        Task<HttpResponseMessage> UpdateAsync(ProductSpecificationAttributeMappingUpdateDto productSpecificationAttributeMappingUpdateDto);
+    }
+}
