404c29e5f27420b775baf693c5f1c5d0778f935f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:06:44 +0200

    Create the IVideoService interface

---
+++ Interfaces/Video/IVideoService.cs
@@ -0,0 +1,27 @@
+﻿
+using nopCommerceWebApiClient.Objects.Video;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Video
+{
+    public interface IVideoService
+    {
+        [Post("/api/video")]
+        Task<VideoDto> CreateAsync(VideoCreateDto createVideoDto);
+
+        [Delete("/api/video/{id}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/video")]
+        Task<IEnumerable<VideoDto>> GetAllAsync();
+
+        [Get("/api/video/{id}")]
+        Task<VideoDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Get("/api/video/url/{url}")]
+        Task<VideoDto> GetByUrlAsync([AliasAs("url")] string url);
+
+        [Put("/api/video")]
+        Task<VideoDto> UpdateAsync(VideoUpdateDto updateVideoDto);
+    }
+}
