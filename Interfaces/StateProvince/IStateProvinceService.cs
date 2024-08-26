f14b5e99767aa2543f19749703e08e53cda76944
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:05:59 +0200

    Create the IStateProvinceService interface

---
+++ Interfaces/StateProvince/IStateProvinceService.cs
@@ -0,0 +1,11 @@
+﻿using nopCommerceWebApiClient.Objects.State;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.StateProvince
+{
+    public interface IStateProvinceService
+    {
+        [Get("/api/state-province")]
+        Task<IEnumerable<StateProvinceDto>> GetAllAsync();
+    }
+}
