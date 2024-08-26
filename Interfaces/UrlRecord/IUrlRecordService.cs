896964dadebc69162440d04a37e7c334756a557e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:07:01 +0200

    Create the IUrlRecordService service

---
+++ Interfaces/UrlRecord/IUrlRecordService.cs
@@ -0,0 +1,23 @@
+﻿using nopCommerceWebApiClient.Objects.UrlRecord;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.UrlRecord
+{
+    public interface IUrlRecordService
+    {
+        [Post("/api/url-records")]
+        Task<UrlRecordDto> CreateAsync(UrlRecordCreateDto urlRecordCreateDto);
+
+        [Delete("/api/url-records/{id}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/url-records")]
+        Task<IEnumerable<UrlRecordDto>> GetAllAsync();
+
+        [Get("/api/url-records/{id}")]
+        Task<UrlRecordDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Put("/api/url-records")]
+        Task<HttpResponseMessage> UpdateAsync(UrlRecordUpdateDto urlRecordUpdateDto);
+    }
+}
