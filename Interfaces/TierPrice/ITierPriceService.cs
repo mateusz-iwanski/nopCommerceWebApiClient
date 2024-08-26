3b2c789b2d482fb4e88bd9a787faa08fdbcaeb0e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:06:30 +0200

    Create the ITierPriceService interface

---
+++ Interfaces/TierPrice/ITierPriceService.cs
@@ -0,0 +1,26 @@
+﻿using nopCommerceWebApiClient.Objects.TierPrice;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.TierPrice
+{
+    public interface ITierPriceService
+    {
+        [Post("/api/tierprice")]
+        Task<TierPriceDto> CreateAsync(TierPriceCreateDto tierPriceDto);
+
+        [Delete("/api/tierprice/{id}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/tierprice")]
+        Task<IEnumerable<TierPriceDto>> GetAllAsync();
+
+        [Get("/api/tierprice/details")]
+        Task<IEnumerable<TierPriceDetailsDto>> GetAllDetailsAsync();
+
+        [Get("/api/tierprice/{id}")]
+        Task<TierPriceDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Get("/api/tierprice")]
+        Task<HttpResponseMessage> UpdateAsync(TierPriceUpdateDto tierPriceDto);
+    }    
+}
