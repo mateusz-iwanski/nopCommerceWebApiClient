f6f004187e0d12efb694b82083627e681022d633
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:06:12 +0200

    Create the ITaxCategoryService interface

---
+++ Interfaces/TaxCategory/ITaxCategoryService.cs
@@ -0,0 +1,11 @@
+﻿using nopCommerceWebApiClient.Objects.TaxCategory;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces
+{
+    public interface ITaxCategoryService
+    {
+        [Get("/api/taxcategory")]
+        Task<IEnumerable<TaxCategoryDto>> GetAllAsync();
+    }    
+}
