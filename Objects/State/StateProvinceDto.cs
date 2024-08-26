d88bbb32e7be27c3bc5836e4ac48c50b70da1be6
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:08:32 +0200

    Create a DTOs for StateProvince

---
+++ Objects/State/StateProvinceDto.cs
@@ -0,0 +1,45 @@
+﻿using nopCommerceWebApiClient.Objects.Country;
+
+namespace nopCommerceWebApiClient.Objects.State
+{
+    /// <summary>
+    /// StateProvince Data Transfer Object
+    /// </summary>
+    /// <remarks>
+    /// This object should be used only for the get method in controller
+    /// </remarks>
+    public record StateProvinceDto
+    {
+        public virtual int Id { get; init; }
+
+        /// <summary>
+        /// Gets the name
+        /// </summary>
+        public virtual string Name { get; init; }
+
+        /// <summary>
+        /// Gets the abbreviation
+        /// </summary>
+        public virtual string? Abbreviation { get; init; }
+
+        /// <summary>
+        /// Gets the country identifier
+        /// </summary>
+        public virtual int CountryId { get; init; }
+
+        /// <summary>
+        /// Gets a value indicating whether the entity is published
+        /// </summary>
+        public virtual bool Published { get; init; }
+
+        /// <summary>
+        /// Gets the display order
+        /// </summary>
+        public virtual int DisplayOrder { get; init; }
+
+        /// <summary>
+        /// Gets country object
+        /// </summary>
+        public virtual Country.CountryDto Country { get; init; }
+    }
+}
