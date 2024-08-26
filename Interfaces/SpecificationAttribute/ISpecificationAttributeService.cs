e555362de62c2b1678a26d96c762c14fea755712
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:05:45 +0200

    Create the ISpecificationAttributeService interface

---
+++ Interfaces/SpecificationAttribute/ISpecificationAttributeService.cs
@@ -0,0 +1,30 @@
+﻿using nopCommerceWebApiClient.Objects.SpecificationAttribute;
+using nopCommerceWebApiClient.Objects.SpecyficationAttribute;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.SpecificationAttribute
+{
+    public interface ISpecificationAttributeService
+    {
+        [Post("/api/specification-attribute")]
+        Task<SpecificationAttributeDto> CreateAsync(SpecificationAttributeCreateDto specificationAttributeCreateDto);
+
+        [Get("/api/specification-attribute")]
+        Task<IEnumerable<SpecificationAttributeDto>> GetAllAsync();
+
+        [Get("/api/specification-attribute/{id}")]
+        Task<SpecificationAttributeDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Get("/api/specification-attribute/name/{name}")]
+        Task<SpecificationAttributeDto> GetByNameAsync([AliasAs("name")] string name);
+
+        [Put("/api/specification-attribute")]   
+        Task<HttpResponseMessage> UpdateAsync(SpecificationAttributeUpdateDto specificationAttributeUpdateDto);
+
+        [Delete("/api/specification-attribute/{id}")]
+        Task<HttpResponseMessage> DeleteAsync([AliasAs("id")] int id);
+
+        [Get("/api/specification-attribute/detail")]
+        Task<IEnumerable<SpecificationAttributeDetailsDto>> GetAllDetailAsync();
+    }
+}
