8df3504909ccd7b3c485862ba0583c7cc136e18e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 11:40:24 +0200

    Create the IProductTemplateService interface

---
+++ Interfaces/Product/IProductTemplateService.cs
@@ -0,0 +1,11 @@
+﻿using nopCommerceWebApiClient.Objects.ProductTemplate;
+using Refit;
+
+namespace nopCommerceWebApiClient.Interfaces.Product
+{
+    public interface IProductTemplateService
+    {
+        [Get("/api/product/template")]
+        Task<IEnumerable<ProductTemplateDto>> GetAllAsync();
+    }
+}
