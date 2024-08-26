5d4606801acec540891f16e6f0777dd57c74e7ab
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-26 14:08:47 +0200

    Create a DTO for TaxCategory

---
+++ Objects/TaxCategory/TaxCategoryDto.cs
@@ -0,0 +1,21 @@
+﻿namespace nopCommerceWebApiClient.Objects.TaxCategory
+{
+    /// <summary>
+    /// Tax Cateogry Data Transfer Object
+    /// </summary>
+    /// <remarks>
+    /// This object should be used only for the get method in controller
+    /// </remarks>
+    public record TaxCategoryDto
+    {
+        /// <summary>
+        /// Gets the name
+        /// </summary>
+        public string Name { get; init; }
+
+        /// <summary>
+        /// Gets the display order
+        /// </summary>
+        public int DisplayOrder { get; init; }
+    }
+}
