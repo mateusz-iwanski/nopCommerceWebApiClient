ccc41fbacf3d9c7407d640fa92047a6e3f189ced
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-06 18:13:21 +0200

    Init ITaxRateService

---
+++ Interfaces/Tax/ITaxRateService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using nopCommerceWebApiClient.Objects.Tax;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceWebApiClient.Interfaces.Tax
+{
+    public interface ITaxRateService
+    {
+        [Get("/api/taxrate")]
+        Task<IEnumerable<TaxRateDto>> GetAllAsync();
+
+        [Get("/api/taxrate/{id}")]
+        Task<TaxRateDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Get("/api/taxrate/percentage/{percentage}")]
+        Task<IEnumerable<TaxRateDto>> GetByPercentageAsync([AliasAs("percentage")] decimal percentage);
+
+        [Get("/api/taxrate/taxcategory/{taxCategoryId}")]
+        Task<ActionResult<IEnumerable<TaxRateDto>>> GetByTaxCategoryId([AliasAs("taxCategoryId")] int taxCategoryId);
+    }
+}
