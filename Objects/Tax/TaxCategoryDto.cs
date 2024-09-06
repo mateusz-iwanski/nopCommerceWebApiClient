b05ee477211ec5dbdfc1cfeee6ca2385a0091e2c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-06 18:12:58 +0200

    Init TaxCategoryDto

---
+++ Objects/Tax/TaxCategoryDto.cs
@@ -0,0 +1,24 @@
+﻿namespace nopCommerceWebApiClient.Objects.TaxCategory
+{
+    /// <summary>
+    /// Tax Cateogry Data Transfer Object
+    /// </summary>
+    /// <remarks>
+    /// This object should be used only for the get method in controller
+    /// </remarks>
+    public record TaxCategoryDto
+    {
+
+        public int Id { get; init; }
+
+        /// <summary>
+        /// Gets the name
+        /// </summary>
+        public string Name { get; init; }
+
+        /// <summary>
+        /// Gets the display order
+        /// </summary>
+        public int DisplayOrder { get; init; }
+    }
+}
