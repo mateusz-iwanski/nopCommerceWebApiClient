b77fed5748b24270a41366d1e6f76e50e6d1078d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-10 22:14:47 +0200

    Add IDto interface for DTOs with DtoMapper support
    Introduced a new namespace `nopCommerceWebApiClient.Interfaces` and added the `IDto` interface. This interface includes a property `Type Type { get; }` and is intended for DTOs using `DtoMapper`. The interface is documented with XML comments, specifying that the type should be marked with `[JsonIgnore]` in the inherited DTO. Added `System.Text.Json.Serialization` directive to support JSON functionality.

---
+++ Interfaces/IDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace nopCommerceWebApiClient.Interfaces
+{
+    /// <summary>
+    /// This interface is used to implement DTOs that will use DtoMapper.
+    /// </summary>
+    /// <remarks>
+    /// If you want to use DtoMapper on a DTO, the DTO must implement this interface.
+    /// The type must be marked as [JsonIgnore] in the inherited DTO object
+    /// </remarks>
+    public interface IDto
+    {
+        Type Type { get; }
+    }
+}
