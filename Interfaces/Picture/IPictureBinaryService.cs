8c741f2ac41cf3852e8e41161633d0aa5e8dd33b
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-27 16:22:25 +0100

    Add IPictureBinaryService interface for picture binary API
    Introduce IPictureBinaryService in nopCommerceWebApiClient.Interfaces.Picture namespace.
    This interface defines methods for creating, deleting, retrieving, and updating picture binaries using the Refit library.

---
+++ Interfaces/Picture/IPictureBinaryService.cs
@@ -0,0 +1,28 @@
+﻿using nopCommerceWebApiClient.Objects.Picture;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceWebApiClient.Interfaces.Picture
+{
+    public interface IPictureBinaryService
+    {
+        [Post("/api/picture-binary")]
+        Task<HttpResponseMessage> CreateAsync(PictureBinaryCreateDto pictureCreateDto);
+
+        [Delete("/api/picture-binary/{id}")]
+        Task<HttpResponseMessage> DeleteAsync(int id);
+
+        [Get("/api/picture-binary")]
+        Task<IEnumerable<PictureDto>> GetAllAsync();
+
+        [Get("/api/picture-binary/{id}")]
+        Task<PictureDto> GetByIdAsync([AliasAs("id")] int id);
+
+        [Put("/api/picture-binary")]
+        Task<HttpResponseMessage> UpdateAsync(PictureBinaryUpdateDto pictureUpdateDto);
+    }
+}
