8cb69b86be4741ae2da237addec0234cae85b257
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-27 16:22:48 +0100

    Add PictureBinaryDto class for PictureBinaryService
    Introduce PictureBinaryDto in nopCommerceWebApiClient.Objects.Picture.
    This DTO includes Id, PictureId (required), and nullable BinaryData.
    It is designed to facilitate data transfer for PictureBinaryService.

---
+++ Objects/Picture/PictureBinaryDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace nopCommerceWebApiClient.Objects.Picture
+{
+    /// <summary>
+    /// PictureBinaryService Data Transfer Object
+    /// </summary>
+    public class PictureBinaryDto
+    {
+        public virtual int Id { get; set; }
+
+        /// <summary>
+        /// ## PictureId
+        /// ### Gets or sets the picture ID
+        /// </summary>
+        [Required]
+        public virtual int PictureId { get; set; }
+
+        /// <summary>
+        /// ## BinaryData
+        /// ### Gets or sets the binary data of the picture
+        /// *Default = null*
+        /// </summary>
+        public virtual byte[]? BinaryData { get; set; }
+    }
+}
